85bc102f3cd8ed0c2aae9e4bad86f2ab5415df1c
paolomic <p.michetti@list-group.com>
2025-02-14 18:05:14 +0100

    sellside reply

--- tests/cd_buyside.robot
+++ tests/cd_buyside.robot
@@ -6,45 +6,94 @@ Library             ../utils/test/test_cd_bs.py
 
 
 *** Variables ***
-&{opts}             speed=110    run=local    reuse_wsp=yes  save_wsp_onclose=yes  close_all_pages=yes                                                    # list of Suite Options
+&{opts}             speed=110    run=local    reuse_wsp=no  save_wsp_onclose=yes  close_all_pages=yes                                                    # list of Suite Options
+@{addin_list}       MetaMarket  FQ SellSide  UserPages
 #@{addin_list}       MetaMarket  UserPages
 &{req_dict}         path=http://10.91.204.20/login    driver=Chrome
 ${ftqid}            000000000000
 
 
 *** Test Cases ***
-Start New Session
+
+########### Coh Start
+COH Start New Session
+    [Documentation]     launch process
+    [Timeout]           2 minutes
+    ${arg}=             Set Variable  ${EMPTY}
+    &{result}=          evaluate      test_cd_bs.robot_run('do_ss_new_session','${arg}',${opts},'new')  
+    log                               result: ${result}
+    ${info} =           Set Variable  ${result}[info]
+    Should Be Equal As Strings        ${result}[status]     ok
+    
+COH Starting Dialog
+    [Documentation]     select addin list, version
+    [Timeout]           2 minutes
+    ${arg}=             Set Variable  @{addin_list}         # Set Argument
+    &{result}=          evaluate      test_cd_bs.robot_run('do_ss_start_dialog',${arg},${opts}, 'hang') 
+    ${info} =           Set Variable  ${result}[info]
+    Should Be Equal As Strings        ${result}[status]     ok
+
+COH Setting Init
+    [Documentation]     set wsp, trace, level
+    [Timeout]           2 minutes
+    ${arg}=             Set Variable  ${EMPTY}
+    &{result}=          evaluate      test_cd_bs.robot_run('do_ss_setting_init','${arg}',${opts}, 'hang') 
+    ${info} =           Set Variable  ${result}[info]
+    Should Be Equal As Strings        ${result}[status]     ok
+
+COH Start Connection
+    [Documentation]     Enable Connections On, wait for Connection Ready
+    [Timeout]           4 minutes     #slow
+    ${arg}=             Set Variable  ${EMPTY}
+    &{result}=          evaluate      test_cd_bs.robot_run('do_ss_start_connections','${arg}',${opts}, 'hang') 
+    log                               result: ${result}
+    ${info} =           Set Variable  ${result}[info]
+    Should Be Equal As Strings        ${result}[status]     ok
+
+########### Web Start
+
+WEB Start New Session
     [Documentation]     login
     [Timeout]           2 minutes
     ${arg}=             Set Variable  ${EMPTY}
-    &{result}=          evaluate      test_cd_bs.robot_run('do_login_session','${arg}',${opts},'')  
+    &{result}=          evaluate      test_cd_bs.robot_run('do_login_session','${arg}',${opts},'web')  
     log                               result: ${result}
     ${info} =           Set Variable  ${result}[info]
     Should Be Equal As Strings        ${result}[status]     ok
     
-Open Rfq Panel
+WEB Open Rfq Panel
     [Documentation]     Open New Rfq Panel
     [Timeout]           2 minutes
     ${arg}=             Set Variable  ${EMPTY}
-    &{result}=          evaluate      test_cd_bs.robot_run('do_open_rfq','${arg}',${opts},'')  
+    &{result}=          evaluate      test_cd_bs.robot_run('do_open_rfq','${arg}',${opts},'web')  
     log                               result: ${result}
     ${info} =           Set Variable  ${result}[info]
     Should Be Equal As Strings        ${result}[status]     ok
-    
-Send a New Rfq
+
+WEB Send a New Rfq
     [Documentation]     Fill Rfq Panel and Send
     [Timeout]           2 minutes
     ${arg}=             Set Variable  ${EMPTY}
-    &{result}=          evaluate      test_cd_bs.robot_run('do_send_rfq','${arg}',${opts},'')  
+    &{result}=          evaluate      test_cd_bs.robot_run('do_send_rfq','${arg}',${opts},'web')  
     log                               result: ${result}
+    ${info} =           Set Variable  ${result}[info]
+    Should Be Equal As Strings        ${result}[status]     ok
+
+
+COH Reply Coherence - SellSide
+    [Documentation]     
+    [Timeout]           2 minutes
+    ${arg}=             Set Variable  ${EMPTY}
+    &{result}=          evaluate      test_cd_bs.robot_run('do_ss_reply','${arg}',${opts}, 'hang') 
     ${info} =           Set Variable  ${result}[info]
     Should Be Equal As Strings        ${result}[status]     ok
     
-Manage Active Pane Rfq
+WEB Manage Active Pane Rfq
     [Documentation]     Manage Active Rfq
     [Timeout]           5 minutes     #slow
     ${arg}=             Set Variable  ${EMPTY}
-    &{result}=          evaluate      test_cd_bs.robot_run('do_manage_rfq','${arg}',${opts},'')  
+    &{result}=          evaluate      test_cd_bs.robot_run('do_manage_rfq','${arg}',${opts},'web')  
     log                               result: ${result}
     ${info} =           Set Variable  ${result}[info]
     Should Be Equal As Strings        ${result}[status]     ok
+
--- utils/test/test_cd_bs.py
+++ utils/test/test_cd_bs.py
@@ -26,7 +26,29 @@ import utl_dump as ud
 
 
 ######################################################
-###  Session Parameter
+###  Session Coherence Parameter
+COH_PATH =              r'D:\COH_x64_CANDEAL_NEXT\bin\Coherence.exe'                # TODO: JSon File ?
+COH_ADDIN=              ['MetaMarket','RFQ Sell Side', 'UserPages']
+
+COH_WSP=                r"D:\wsp_cd\robot_test.wsp4"
+COH_PRIMARY=            '10.91.204.14'         
+COH_PORT=               '27000'         
+COH_USER=               'AASMAH@RBC'         
+COH_PASS=               '*'         
+COH_BAND_SAVE=           True
+
+#COH_EXC=                'BIT'
+#COH_MRK=                'MTA'
+#COH_SEC=                'AUTOGRILL SPA'
+#COH_PRICE =             '95.01'
+#COH_QTY =               '100'
+#COH_ALIAS =             'KATIA'
+#COH_CLIENTID =          'MARI'
+#COH_CLIENTACC =         'TEST'
+
+
+######################################################
+###  Session Web Parameter
 #region 
 
 WEB_URL =               r'http://10.91.204.20/login'
@@ -38,8 +60,121 @@ WEB_PRICE =             '101.34'
 #endregion
 
 
+
 ######################################################
-### Robot Operations
+### Robot Coherence Operations
+#region 
+def do_ss_new_session(arg):
+    path_wsp = Path(COH_WSP)
+    if (path_wsp.exists() and not opt.get('reuse_wsp')=='yes'):
+        print('Remove Workspace...')
+        path_wsp.unlink()
+        VERIFY(not path_wsp.exists(), 'Wsp Exist')
+    #path_wsp_folder = Path(COH_WSP.replace('.wsp4', '.wsp4_wrk'))
+    #if (path_wsp_folder.exists()):
+    #    path_wsp_folder.rmdir()
+    #VERIFY(not path_wsp_folder.exists(), 'Wsp Folder Exist')
+
+def do_ss_start_dialog(arg):
+    edit = uw.get_child_chk(env.wtop, automation_id='12429', ctrl_type='Edit', deep=3)
+    uw.edit_set(edit, COH_WSP)
+
+    list = uw.get_child_chk(env.wtop, name='Import From', ctrl_type='List', deep=3)
+    uw.list_check(list, '*', False)
+    uw.list_check(list, COH_ADDIN, True)
+
+    butt = uw.get_child_chk(env.wtop, automation_id='1', ctrl_type='Button', deep=3)
+    if not opt.get('reuse_wsp')=='yes':
+        VERIFY(butt.window_text()=='Create', 'Can`t Create New Workspace')
+
+    uw.win_click(butt, wait_end=0.5)
+    uw.warning_replay('This workspace has not been closed properly', 'No')
+                                                                                   
+    env.reload()                                                                            # Smart Wait for new Main Frame   
+        
+def do_ss_setting_init(arg):
+    butt = uw.get_child_chk(env.wtop, name='Settings', ctrl_type='Button', deep=4)          # Settings gia aperto se New Wsp
+    uw.win_click(butt)
+
+    pane = uw.get_child_chk(env.wtop, name='Settings', ctrl_type='Pane', deep=3)
+    list = uw.get_child_chk(pane, automation_id='103', ctrl_type='List', deep=3)
+
+    ### Connection  - Disable se connection Ready
+    try:
+        edit = uw.get_child_chk(pane, name='Primary', ctrl_type='Edit', deep=3)
+        uw.edit_set(edit, COH_PRIMARY)
+        edit = uw.get_child_chk(pane, name='Port', ctrl_type='Edit', deep=3)
+        uw.edit_set(edit, COH_PORT)
+        edit = uw.get_child_chk(pane, name='User name', ctrl_type='Edit', deep=3)
+        uw.edit_set(edit, COH_USER)
+        edit = uw.get_child_chk(pane, automation_id='11303', ctrl_type='Edit', deep=3)
+        uw.edit_set(edit, COH_PASS)
+        
+        if COH_BAND_SAVE:
+            butt = uw.get_child_chk(pane, name='Bandwidth Saving', ctrl_type='CheckBox', deep=3)
+            if not uw.butt_is_checked(butt):
+                uw.win_click(butt)
+    except:
+        print('Connection is Started')
+
+    ### MetaMarket
+    uw.list_select(list, "MetaMarket")
+
+    trace = uw.get_child_chk(pane, name='Trace Level', ctrl_type='Custom', deep=3)
+    uw.win_click(trace, mode='combo')
+
+    #sleep(.25)
+    #ud.dump_uia_tree(env.wtop)         # non c'e' lista popup
+
+    uw.hide_select(-1)                  # Todo Control Inside
+    keyboard.press("enter")             # Confirm Selection
+    sleep(.25)
+
+    ### WorkSpace
+    uw.list_select(list, "Workspace")
+
+    combo = uw.get_child_chk(pane, automation_id='11347', ctrl_type='ComboBox', deep=3)         # Todo: Input Per Valore
+    uw.win_click(combo)
+    keyboard.press("end")
+    keyboard.press("enter")
+
+    combo = uw.get_child_chk(pane, automation_id='11345', ctrl_type='ComboBox', deep=3)
+    uw.win_click(combo)
+    keyboard.press("end")
+    keyboard.press("enter")
+
+    ### OK
+    butt = uw.get_child_chk(pane, name='OK', ctrl_type='Button', deep=3)
+    uw.win_click(butt)
+
+def do_ss_start_connections(arg):
+    if opt.get('close_all_pages')=='yes':
+        uw.page_close_all()
+    
+    addins = ['MetaMarket']                                                     # TODO pass form Robot ?
+    for addin in addins:
+        butt = env.select_ribbon_butt(addin, 'Auto Connect')
+        if not uw.butt_is_checked(butt):
+            uw.win_click(butt)
+
+    butt = env.select_ribbon_butt('Home', 'Auto Connect')
+    if not uw.butt_is_checked(butt):
+        uw.win_click(butt)
+
+    if env.wait_conn_ready(to_sec=120, to_err_sec=5, delay=2):
+        print ('Connection Ready')
+    else:
+        RAISE("Connection Fail")
+
+def do_ss_reply(arg):
+    dlg_rfq = uw.get_child_chk(env.wtop, name='RFQ Outright \[.*\] \[.*\]', ctrl_type='Pane', deep=1, use_re=1)  
+    butt = uw.get_child_chk(dlg_rfq, name='Done', ctrl_type='Button', deep=1)  
+    uw.win_click(butt)
+#endregion
+
+
+######################################################
+### Robot Web Operations
 #region 
 
 def do_login_session(arg):
@@ -102,7 +237,6 @@ def do_send_rfq(arg):
     sleep(1)
 #endregion
 
-
 def do_manage_rfq(arg):
 
     (rfq, table, grp) = webapp.hang_rfq()
@@ -148,7 +282,6 @@ def do_manage_rfq(arg):
     print(tb.rows)
 
 
-
 ######################################################
 # Generic Caller
 
@@ -156,15 +289,31 @@ def do_manage_rfq(arg):
 
 
 def robot_run(fun_name:str, arg:str='', options=[], s_op=''):
-    def manage_session(s_op):
-        pass
-    def verify_session(ope):
-        pass
+    def manage_session(session):
+        if session=='new':
+            env.launch_app(COH_PATH)
+        else:
+            env.hang_app(COH_PATH)
+        if session=='kill':
+            uw.session_close(env.wtop, wait_init=1, wait_end=1, logoff=True)
+    def verify_session(session):
+        if session=='kill':
+            exist = 0
+            try:
+                env.hang_app(COH_PATH)
+                exist=1
+            except Exception as e:
+                pass
+            VERIFY(not exist, 'Close Session Failed. Process still Exists')
+        else:
+            VERIFY(env.app and env.wtop, "Hang or New Session Failed")
+            env.wtop.set_focus()
     try:
         opt.set(options)
         print(f'Test Options: {options}')
-        manage_session(s_op)
-        verify_session(s_op)
+        if s_op and s_op != 'web':
+            manage_session(s_op)   
+            verify_session(s_op)
         func = globals().get(fun_name)
         result = func(arg)
         return ROBOT_RES('ok', result)
@@ -179,15 +328,16 @@ def robot_run(fun_name:str, arg:str='', options=[], s_op=''):
 
     
 if __name__ == '__main__':
-    opts = {'speed': '110', 'run': 'local', 'reuse_wsp': 'yes', 'save_wsp_onclose': 'yes', 'close_all_pages': 'yes'}
+    opts = {'speed': '110', 'run': 'local', 'reuse_wsp': 'no', 'save_wsp_onclose': 'yes', 'close_all_pages': 'yes'}
     opt.set(opts)
-    select = 2
+    select = 1
     if (select==1):
         #print(robot_run('do_login_session', '', opts, '') )
         #print(robot_run('do_open_rfq', '', opts, '') )
         #print(robot_run('do_send_rfq', '', opts, '') )
-        print(robot_run('do_manage_rfq', '', opts, '') )
-        #do_grid_sample('')
+        #print(robot_run('do_manage_rfq', '', opts, '') )
+        #print(robot_run('do_ss_new_session', '', opts, 'new') )
+        print(robot_run('do_ss_reply', '', opts, 'hang') )
         pass
     if (select==2):
         do_manage_rfq('')
